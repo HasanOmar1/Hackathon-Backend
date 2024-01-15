e903c7a0bce3915672b5b224c2348c863b2571be
fadyzar <fadyzarka978@gmail.com>
2024-01-15 10:24:06 +0200

    added

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,20 +1,18 @@
 import getOpenAiInstance from "../openAI/openAI.js";
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export const chat = async (req, res) => {
   const userInput = req.body.message;
   const language = req.body.language;
   const systemMessage = {
     role: "system",
-    content: `You are a smart lawyer and an attorney , answer the questions that u get in this language ${language} .`,
+    content: `You are a smart lawyer and an attorney , answer the questions that u get in this language ${language} . and ask me questions to provide you all the details you need.
+    You can't help with things outside the realm of lawyers. `,
   };
   const conversation = [systemMessage];
-  // [
-  //   {role: "system", content: "You are a helpful assistant."},
-  //   {role: "user", content: "Hi"},
-  //   {role: "assistant", content: "Hello! How can I assist you today?"},
-  //   {},
-
-  // ]
 
   try {
     conversation.push({ role: "user", content: userInput });
@@ -24,7 +22,7 @@ export const chat = async (req, res) => {
       model: "gpt-3.5-turbo",
       messages: conversation,
       max_tokens: 300,
-      temperature: 1,
+      temperature: 1.7,
     });
 
     const assistantResponse = response.choices[0].message;
@@ -35,3 +33,28 @@ export const chat = async (req, res) => {
     res.status(500).send("Error generating text");
   }
 };
+
+export const speech = async (req, res) => {
+  const openai = getOpenAiInstance();
+  const text = req.body.text;
+  const voice = req.body.voice || "echo";
+
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const speechFile = path.resolve(__dirname, "../../speech.mp3");
+
+  try {
+    const mp3 = await openai.audio.speech.create({
+      model: "tts-1",
+      voice: voice,
+      input: text,
+    });
+
+    const buffer = Buffer.from(await mp3.arrayBuffer());
+    await fs.writeFile(speechFile, buffer);
+
+    res.download(speechFile);
+  } catch (error) {
+    res.status(500).send("Error in generating speech.");
+  }
+};
+
--- routes/chatRoute.js
+++ routes/chatRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { chat } from "../controllers/chatController.js";
+import { speech } from '../controllers/chatController.js';
 
 const router = express.Router();
 
 router.post("/", chat);
+
 
 export default router;
