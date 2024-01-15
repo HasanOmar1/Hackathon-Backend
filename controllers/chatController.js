a67a1dc3aa05ef45a00c5b02610131aba654c4cd
HasanOmar1 <r3domar1@gmail.com>
2024-01-15 14:17:22 +0200

    updated

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,65 +1,54 @@
 import getOpenAiInstance from "../openAI/openAI.js";
-import fs from "fs/promises";
-import path from "path";
-import { fileURLToPath } from "url";
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export const chat = async (req, res) => {
   const userInput = req.body.message;
   const language = req.body.language;
+  const descriptionInput = req.body.description;
+  const dateInput = req.body.date;
+
   const systemMessage = {
     role: "system",
-
-    content: `You are a smart lawyer and an attorney , answer the questions that u get in this language ${language} . and ask me questions to provide you all the details you need.
-    You can't help with things outside the realm of lawyers. `,
+    /* Legal Bot, I need assistance in drafting a concise description for my legal case. The case involves ${descriptionInput}. Please include key details such as ${dateInput}, and any other information crucial to understanding the case. Additionally, highlight the main legal issues at play and any specific laws or regulations applicable. Your help in creating a clear and informative case description is appreciated. */
+    content: `I need assistance in drafting a concise description for my legal case. The case involves ${descriptionInput}.
+     Please include key details such as ${dateInput}, 
+     and any other information crucial to understanding the case. 
+     Additionally, highlight the main legal issues at play and any specific laws 
+     or regulations applicable. 
+    Your help in creating a clear and informative case description is appreciated. 
+    Focus on answering questions related to ${language} law 
+    and feel free to ask for additional details to provide comprehensive responses. 
+    Your expertise is limited to legal matters, 
+    and refrain from making jokes or engaging in non-legal discussions. 
+    and dont make jokes.
+    Your goal is to assist users with precise and accurate legal 
+    information.`,
   };
+
   const conversation = [systemMessage];
 
   try {
     conversation.push({ role: "user", content: userInput });
 
+    // Include description and date inputs in the conversation
+    if (descriptionInput) {
+      conversation.push({
+        role: "user",
+        content: `Description: ${descriptionInput}`,
+      });
+    }
+
+    if (dateInput) {
+      conversation.push({ role: "user", content: `Date: ${dateInput}` });
+    }
+
     const openai = getOpenAiInstance();
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: conversation,
-
       max_tokens: 300,
       temperature: 1.7,
-
-      max_tokens: 500,
-      temperature: 1,
-      presence_penalty: 1,
     });
-
-    const assistantResponse = response.choices[0].message;
-
-    conversation.push(assistantResponse);
-    return res.send(assistantResponse.content);
   } catch (error) {
     res.status(500).send("Error generating text");
-  }
-};
-
-export const speech = async (req, res) => {
-  const openai = getOpenAiInstance();
-  const text = req.body.text;
-  const voice = req.body.voice || "echo";
-
-  const __dirname = path.dirname(fileURLToPath(import.meta.url));
-  const speechFile = path.resolve(__dirname, "../../speech.mp3");
-
-  try {
-    const mp3 = await openai.audio.speech.create({
-      model: "tts-1",
-      voice: voice,
-      input: text,
-    });
-
-    const buffer = Buffer.from(await mp3.arrayBuffer());
-    await fs.writeFile(speechFile, buffer);
-
-    res.download(speechFile);
-  } catch (error) {
-    res.status(500).send("Error in generating speech.");
   }
 };
